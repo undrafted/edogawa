e207aa9333eeef9b669d73d7e63a6cbff1f5f828
undrafted <8jermiedomingo@gmail.com>
2020-03-22 14:58:03 +0100

    add isSriptError to lib

---
+++ src/lib/dom.ts
@@ -0,0 +1,4 @@
+// to isolate script errors
+//When an error occurs in a script, loaded from a different origin, the details of the error are not reported to prevent leaking information
+//the error detail is only viewable in the browser console and not accessible via JavaScript.
+export const isSriptError = (msg: string) => msg.toLowerCase().indexOf('script error') > -1;
