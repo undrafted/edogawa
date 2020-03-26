6cfb8bbb6f92b743521bd773db2385b806582e53
undrafted <8jermiedomingo@gmail.com>
2020-03-26 21:42:40 +0100

    add helpers.spec

---
+++ src/lib/helpers.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { truncateString } from './helpers';
+
+describe('truncateString', function() {
+  it('truncate long string', () => {
+    const lorem =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+    expect(truncateString(lorem)).to.equal('Lorem ipsum dolor sit amet, consect...');
+    expect(truncateString(lorem, 50)).to.equal(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing...',
+    );
+  });
+});
