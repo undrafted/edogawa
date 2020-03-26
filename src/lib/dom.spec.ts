56d10de2c1e1dbf73e1362271ec28f30648bbdc0
undrafted <8jermiedomingo@gmail.com>
2020-03-26 21:38:29 +0100

    add dom.spec

---
+++ src/lib/dom.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { isSriptError } from './dom';
+
+describe('isSriptError', function() {
+  it('should return true for script errors', () => {
+    expect(isSriptError('Example Script Error')).to.be.true;
+  });
+  it('should return false for non script errors', () => {
+    expect(isSriptError('Type error at line...')).to.be.false;
+  });
+});
