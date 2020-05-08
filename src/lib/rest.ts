c6bfaa1326b6c5898278f25af8190a1a3756476b
undrafted <8jermiedomingo@gmail.com>
2020-05-08 20:53:12 +0200

    add fetch polyfill

--- src/lib/rest.ts
+++ src/lib/rest.ts
@@ -9,6 +9,8 @@ export class RestClient {
 
   post(params: any) {
     try {
+      const fetch = window.fetch || fetchPolyfill;
+
       fetch(this.endPoint, {
         method: 'POST',
         body: JSON.stringify({
@@ -17,7 +19,24 @@ export class RestClient {
         }),
       });
     } catch (e) {
-      console.log('failed to push edogawa exception: ', e);
+      console.log('Failed to push edogawa exception: ', e);
     }
   }
 }
+
+const fetchPolyfill = (url: string, options: RequestInit = {}) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(options.method || 'GET', url);
+    xhr.onload = () => {
+      resolve({
+        status: xhr.status,
+        statusText: xhr.statusText,
+      });
+    };
+
+    xhr.onerror = (e) => reject(e);
+    xhr.send(options.body);
+  });
+};
